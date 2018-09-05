217f90b057d246fded42b0c614e5ce750364899a
Bayron Rodezno <bayron.r@abposus.com>
2018-09-05 16:40:16 -0500

    First Advance Menu Categories Task

---
+++ aybPaymentSolutionApp/aybPaymentSolutionApp/ViewModel/MenuBackMenuVM.cs
@@ -0,0 +1,33 @@
+﻿using aybPaymentSolutionApp.View.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace aybPaymentSolutionApp.ViewModel
+{
+	public class MenuBackMenuVM
+	{
+
+        public Command RedirectCommand { get; set; }
+        public INavigation navigation { get; set; }
+
+		public MenuBackMenuVM ()
+		{
+            RedirectCommand = new Command<string>(doRedirect);
+		}
+
+
+        private void doRedirect(string opcionMenu)
+        {
+            switch (opcionMenu.ToString())
+            {
+                case "RegCat":
+                    navigation.PushModalAsync(new CategoriesMenuList());
+                    break;
+            }
+
+        }
+	}
+}
